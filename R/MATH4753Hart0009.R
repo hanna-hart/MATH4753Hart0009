d79cb7b48f8969001b04d597c7d33c93bdabc125
Hanna Hart <hannaahart5@gmail.com>
2020-09-17 22:19:50 -0500

    first commit

---
+++ R/MATH4753Hart0009.R
@@ -0,0 +1,10 @@
+#'BMMI Package
+#'
+#'Contains one function for creating BMI groups.
+#'
+#'@docType package
+#'
+#'@author Hanna Hart \email{hannaahart5@gmail.com}
+#'
+#'@name MATH4753Hart0009
+NULL
---
+++ R/bmi3.R
@@ -0,0 +1,18 @@
+#' Create BMI Groups
+#'
+#' More detailed description.
+#'
+#' @param x Numeric vector
+#'
+#' @return Factor variable
+#'
+#' @examples
+#' bmi.vals <- rnorm(n = 20, mean = 25, sd = 3)
+#' bmi3(bmi.vals)
+#'
+#' @export
+
+bmi3 <- function(x) {
+  bmi.groups <- cut(x, breaks = c(0, 25, 30, Inf), right = FALSE)
+  return(bmi.groups)
+}
