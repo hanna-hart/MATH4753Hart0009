c8f3e96b7994c62a50c785e57e97d6e791a9266b
Hanna Hart <hannaahart5@gmail.com>
2020-10-09 22:54:03 -0500

    add fire.rda and fire.r

---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' FIREDAM data set.
+#'
+#' A data set collecting the number of fires and their damage
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{Distance}
+#'   \item{Damage}
+#'   ...
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
+
